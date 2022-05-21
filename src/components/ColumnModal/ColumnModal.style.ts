3088ff1e66cbab8c4f0adedc01ec41821c6fbff9
Kristoffer Linderman <kristoffer.linderman@bygghemma.se>
2022-05-21 18:22:53 +0200

    Add ColumnModal

---
+++ src/components/ColumnModal/ColumnModal.style.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  border: 1px solid #333;
+  border-radius: 4px;
+  cursor: pointer;
+`;
---
+++ src/components/ColumnModal/ColumnModal.tsx
@@ -0,0 +1,70 @@
+import { useEffect, useState } from "react";
+import { Column } from "../../types";
+import { v4 as uuidv4 } from "uuid";
+import { TextInput } from "../common/Input";
+import { Modal } from "../common/Modal";
+import { Container } from "./ColumnModal.style";
+
+type ColumnModalProps = {
+  column: Column | null;
+  isOpen: boolean;
+  onCancel: () => void;
+  onSubmit: (column: Column) => void;
+  isEditMode: boolean;
+};
+
+const ColumnModal = ({
+  column,
+  isOpen,
+  onCancel,
+  onSubmit,
+  isEditMode,
+}: ColumnModalProps) => {
+  const [title, setTitle] = useState("");
+
+  const reset = () => {
+    setTitle("");
+  };
+
+  useEffect(() => {
+    if (column) {
+      setTitle(column.title);
+    }
+  }, [column]);
+
+  const handleSubmit = () => {
+    const newColumn: Column = {
+      title,
+      id: column?.id || uuidv4(),
+      tasks: [],
+    };
+
+    reset();
+    onSubmit(newColumn);
+  };
+
+  const handleCancel = () => {
+    reset();
+    onCancel();
+  };
+
+  return (
+    <Container>
+      <Modal
+        isOpen={isOpen}
+        submitLabel={isEditMode ? "Edit Column" : "Add Column"}
+        onCancel={() => handleCancel()}
+        onSubmit={() => handleSubmit()}
+        title={isEditMode ? "Edit Column" : "Add Column"}
+      >
+        <TextInput
+          label="Column title"
+          onChange={(value) => setTitle(value)}
+          value={title}
+        />
+      </Modal>
+    </Container>
+  );
+};
+
+export { ColumnModal };
